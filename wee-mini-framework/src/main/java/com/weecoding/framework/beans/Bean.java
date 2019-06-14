42dcafd40b7f3f3ebcf5a9c4b77e735c8a740604
wuy <1311695042@qq.com>
2019-06-14 15:33:58 +0800

    修改bean注解

--- wee-mini-framework/src/main/java/com/weecoding/framework/beans/Bean.java
+++ wee-mini-framework/src/main/java/com/weecoding/framework/beans/Bean.java
@@ -10,6 +10,6 @@ import java.lang.annotation.*;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target(ElementType.TYPE)
 public @interface Bean {
 }
